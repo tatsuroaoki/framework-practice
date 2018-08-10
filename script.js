d5e98aff9fb689813f3fff793a3c48a60aa01647
tatsuroaoki <tatsuro.aoki@gmail.com>
2018-08-09 19:03:10 -0700

    Hello DUDE message

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
 var gators = [
-  {name: "Elvis Vessley",
-    hobbies: "surfing, dancing",
-    weight: 865,
-    likesIceCream: true,
-    imgSource: "https://img-aws.ehowcdn.com/877x500p/s3-us-west-1.amazonaws.com/contentlab.studiod/getty/10c413dcc4d444acbd6810f483028933"
-  },{name: "Little Britches",
-    hobbies: "shaking, eating fruit",
-    weight: 361,
-    likesIceCream: true,
-    imgSource: "http://nebula.wsimg.com/5df260927e75521ad0f18fae842e522e?AccessKeyId=00569247111AAAE64481&disposition=0&alloworigin=1"
-  },{name: "Wanda Snapson",
-    hobbies: "having parties",
-    weight: 712,
-    likesIceCream: false,
-    imgSource: "http://www.vaguebuttrue.com/images/1449775760-alligatorclimbsfenceWEBSITE.jpg"
-  }
+	{name: "Elvis Vessley",
+		hobbies: "surfing, dancing",
+		weight: 865,
+		likesIceCream: true,
+		imgSource: "https://img-aws.ehowcdn.com/877x500p/s3-us-west-1.amazonaws.com/contentlab.studiod/getty/10c413dcc4d444acbd6810f483028933"
+	},{name: "Little Britches",
+				hobbies: "shaking, eating fruit",
+				weight: 361,
+				likesIceCream: true,
+				imgSource: "http://nebula.wsimg.com/5df260927e75521ad0f18fae842e522e?AccessKeyId=00569247111AAAE64481&disposition=0&alloworigin=1"
+			},{name: "Wanda Snapson",
+						hobbies: "having parties",
+						weight: 712,
+						likesIceCream: false,
+						imgSource: "http://www.vaguebuttrue.com/images/1449775760-alligatorclimbsfenceWEBSITE.jpg"
+					}
 ]
+
+document.addEventListener("DOMContentLoaded", function() {
+	var app = new Vue({
+		el: '#app',
+		data: {
+			message: 'Hello DUDE!'
+		}
+	})
+	})
